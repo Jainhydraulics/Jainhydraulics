a5d0fa052d60f68d7ea3459034fa864b9665937a
fff <fdfd@gmail.com>
2024-01-24 16:53:27 +0530

    added astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import partytown from "@astrojs/partytown";
 
 //https://github.com/BuilderIO/partytown/issues/72 for ganalytics
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		mdx(),
-		sitemap()
-	],
-	output: "static",
-	prefetch: {
-		defaultStrategy: "viewport",
-		prefetchAll: true,
-	},
-	site: "https://jainhydraulics.com",
+  integrations: [tailwind(), mdx(), sitemap(), compress()],
+  output: "static",
+  prefetch: {
+    defaultStrategy: "viewport",
+    prefetchAll: true
+  },
+  site: "https://jainhydraulics.com"
 });
-
-
